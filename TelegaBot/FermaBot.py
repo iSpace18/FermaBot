c3a03376c5943282d4edea30e6b1293823acb5c1
Timur <bendelianitimur2007@gmail.com>
2024-04-29 22:50:31 +0300

    врывр

---
+++ TelegaBot/FermaBot.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+import json
+
+bot = Bot('токен')
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start']) 
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(url='https://koracon.github.io/VSC/?')))
+    await message.answer('Привет, мой друг', reply_markup=markup)
+
+@dp.message_handler(content_types=['web_app_data'])
+async def web_app(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'Имя: {res["name"]}. Email: {res["email"]}.Номер телефона: {res["phone"]}')
+
+executor.start_polling(dp)
