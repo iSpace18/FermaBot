85ed3520c0ce1d3200a8a7326889488161f67374
iSpace18 <steamdavid2007@gmail.com>
2024-04-30 19:05:10 +0300

    134134

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ let counter = 0;
 const counterElement = document.getElementById('counter');
 const clickButton = document.getElementById('clickButton');
 
-clickButton.addEventListener('click', () => {
+document.documentElement.ondragstart = () => false;
+
+clickButton.addEventListener('click', (event) => {
     counter++;
-    counterElement.innerText = counter;
-
-
+    counterElement.innerHTML = `<img class="coin" src="images/Coin-PNG-Picture.png" alt="" /> ${counter}`;
+    
     const animation = document.createElement('div');
     animation.classList.add('animation');
     animation.innerText = '+1';
     document.body.appendChild(animation);
-
-    setTimeout(() => {
+    
+    animation.style.position = 'absolute';
+    animation.style.left = event.clientX + 'px';
+    animation.style.top = event.clientY + 'px';
+    
+    animation.style.animation = 'floatUp 1s forwards';
+    
+    animation.addEventListener('animationend', () => {
         document.body.removeChild(animation);
-    }, 1000);
+    });
 });
